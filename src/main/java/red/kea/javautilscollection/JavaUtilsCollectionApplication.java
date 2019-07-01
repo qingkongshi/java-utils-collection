c71fd83c7e5bd680eedd23f001a3b3e91c3ed41e
lihao <734682120@qq.com>
2019-07-01 15:11:29 +0800

    创建项目

---
+++ src/main/java/red/kea/javautilscollection/JavaUtilsCollectionApplication.java
@@ -0,0 +1,13 @@
+package red.kea.javautilscollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUtilsCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUtilsCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/red/kea/javautilscollection/JavaUtilsCollectionApplicationTests.java
@@ -0,0 +1,16 @@
+package red.kea.javautilscollection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaUtilsCollectionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
