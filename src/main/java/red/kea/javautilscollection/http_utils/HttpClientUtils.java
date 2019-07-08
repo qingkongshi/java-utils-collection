131ee0eb273f8fdbaea606e689942495a29038b5
lihao <734682120@qq.com>
2019-07-08 15:08:34 +0800

    Http请求：使用HttpClient发送get请求

---
+++ src/main/java/red/kea/javautilscollection/http_utils/HttpClientUtils.java
@@ -0,0 +1,148 @@
+package red.kea.javautilscollection.http_utils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * @ClassName HttpClientUtils
+ * @Author KeA
+ * @Date 2019/7/8 10:32
+ * @Version 1.0
+ * @Method
+ * 实现了所有 HTTP 的方法（GET、POST、PUT、HEAD、DELETE、HEAD、OPTIONS 等）
+ * 支持 HTTPS 协议
+ * 支持代理服务器（Nginx等）等
+ * 支持自动（跳转）转向
+ */
+public class HttpClientUtils {
+    /**
+     * 引入httpclient依赖
+     * <dependency>
+     *      <groupId>org.apache.httpcomponents</groupId>
+     *      <artifactId>httpclient</artifactId>
+     * </dependency>
+     *
+     * 引入fasljson依赖
+     * <dependency>
+     *      <groupId>com.alibaba</groupId>
+     *      <artifactId>fastjson</artifactId>
+     *      <version>1.2.51</version>
+     * </dependency>
+     */
+
+
+    /**
+     * GET---无参测试
+     */
+    public void doGetNoParam() {
+        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        // 创建Get请求
+        HttpGet httpGet = new HttpGet("http://localhost:12345/doGetControllerOne");
+
+        // 响应模型
+        CloseableHttpResponse response = null;
+        try {
+            // 由客户端执行(发送)Get请求
+            response = httpClient.execute(httpGet);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+            System.out.println("响应状态为:" + response.getStatusLine());
+            if (responseEntity != null) {
+                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+                System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeIO(httpClient,response);
+        }
+    }
+    /**
+     * GET---有参测试 (方式一:手动在url后面加上参数)
+     *
+     */
+    public void doGetWithParam() {
+        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        // 参数
+        StringBuffer params = new StringBuffer();
+        try {
+            // 字符数据最好encoding以下;这样一来，某些特殊字符才能传过去(如:某人的名字就是“&”,不encoding的话,传不过去)
+            params.append("name=" + URLEncoder.encode("&", "utf-8"));
+            params.append("&");
+            params.append("age=24");
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+
+        // 创建Get请求
+        HttpGet httpGet = new HttpGet("http://localhost:12345/doGetControllerTwo" + "?" + params);
+        // 响应模型
+        CloseableHttpResponse response = null;
+        try {
+            // 配置信息
+            RequestConfig requestConfig = RequestConfig.custom()
+                    // 设置连接超时时间(单位毫秒)
+                    .setConnectTimeout(5000)
+                    // 设置请求超时时间(单位毫秒)
+                    .setConnectionRequestTimeout(5000)
+                    // socket读写超时时间(单位毫秒)
+                    .setSocketTimeout(5000)
+                    // 设置是否允许重定向(默认为true)
+                    .setRedirectsEnabled(true).build();
+
+            // 将上面的配置信息 运用到这个Get请求里
+            httpGet.setConfig(requestConfig);
+
+            // 由客户端执行(发送)Get请求
+            response = httpClient.execute(httpGet);
+
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+            System.out.println("响应状态为:" + response.getStatusLine());
+            if (responseEntity != null) {
+                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+                System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeIO(httpClient,response);
+        }
+    }
+
+    private void closeIO(CloseableHttpClient httpClient,CloseableHttpResponse response){
+        try {
+            // 释放资源
+            if (httpClient != null) {
+                httpClient.close();
+            }
+            if (response != null) {
+                response.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
