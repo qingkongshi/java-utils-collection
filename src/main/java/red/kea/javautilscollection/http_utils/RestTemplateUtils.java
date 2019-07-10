4343d8a3656297f45edeaceaa2e1337ec2b63722
lihao <734682120@qq.com>
2019-07-10 17:46:38 +0800

    Http请求：使用RestTemplate发送请求

---
+++ src/main/java/red/kea/javautilscollection/http_utils/RestTemplateUtils.java
@@ -0,0 +1,16 @@
+package red.kea.javautilscollection.http_utils;
+
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName RestTemplateUtils
+ * @Author KeA
+ * @Date 2019/7/9 9:58
+ * @Version 1.0
+ */
+public class RestTemplateUtils {
+    public void test(){
+        RestTemplate restTemplate = new RestTemplate();
+//        restTemplate.get
+    }
+}
