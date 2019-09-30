1901880c04c94c7a86eb41efebc53c6f3d057d6c
lihao <734682120@qq.com>
2019-09-30 10:49:38 +0800

    保持更新

--- src/main/java/red/kea/javautilscollection/JavaUtilsCollectionApplication.java
+++ src/main/java/red/kea/javautilscollection/JavaUtilsCollectionApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaUtilsCollectionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JavaUtilsCollectionApplication.class, args);
 	}
