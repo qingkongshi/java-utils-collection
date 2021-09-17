c42a7b20c18427210f2941b0d209675b4e06fa8b
KeA <734682120@qq.com>
2021-09-17 17:27:02 +0800

    五子棋

---
+++ src/main/java/red/kea/javautilscollection/game/gobang/Main.java
@@ -0,0 +1,16 @@
+package red.kea.javautilscollection.game.gobang;
+
+/**
+ * @author： KeA
+ * @date： 2021-09-03 16:31:31
+ * @version: 1.0
+ * @describe:
+ */
+public class Main {
+    /** 启动函数
+     * @param args
+     */
+    public static void main(String[] args) {
+        new Windows("五子棋");
+    }
+}
